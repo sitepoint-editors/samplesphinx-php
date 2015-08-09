8db2473734fba3f0fb032b07ba5c70eeac52abe6
Bruno Skvorc <bruno@skvorc.me>
2015-08-09 21:58:52 +0000

    Trying out php domain again

--- source/conf.py
+++ source/conf.py
@@ -29,8 +29,8 @@ from recommonmark.parser import CommonMarkParser
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = ["sphinxcontrib.phpdomain"]
-extensions = []
+extensions = ["sphinxcontrib.phpdomain"]
+#extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
